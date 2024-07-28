90f2cfa02855f312175051163cc48a8fcba1ed1d
Vedasitoleh <vedanjali.sitoleh@g.austincc.edu>
2024-07-28 04:29:46 +0000

    final

--- src/question_a/main.py
+++ src/question_a/main.py
@@ -1,1 +1,12 @@
 #add import
+
+import question_a
+
+user_input = 'Y'
+
+while user_input != 'quit':
+    user_input = input("Enter 'quit' to exit program: \n")
+    question_a.display_multiplication_table(question_a.create_multiplication_table(1))
+    
+    
+
--- src/question_a/question_a.py
+++ src/question_a/question_a.py
@@ -1,3 +1,20 @@
 #write functions here, don't add input('') statements here!
 def test_config():
     return True
+
+def create_multiplication_table(num):
+    final_table = []
+    num = 1
+   
+    while num <= 5:
+        table = [ int(num) * i for i in range(1,11)]
+        num += 1
+        final_table = final_table + table
+
+    return final_table
+
+def display_multiplication_table(final_table):
+    
+    print(final_table)
+
+display_multiplication_table(create_multiplication_table(1))
